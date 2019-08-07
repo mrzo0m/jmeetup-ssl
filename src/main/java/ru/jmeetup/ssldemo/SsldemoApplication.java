97ca026006498c3abb4615416f49d11502c166d3
Burshinov.O.Al <Burshinov.O.Al@sberbank.ru>
2019-08-07 14:33:17 +0300

    Spring init

---
+++ src/main/java/ru/jmeetup/ssldemo/SsldemoApplication.java
@@ -0,0 +1,13 @@
+package ru.jmeetup.ssldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsldemoApplication.class, args);
+	}
+
+}
